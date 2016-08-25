9ea398931a942792e85707d62510be6d116553fe
YoshikiIkegaya <mr.ikegaya@gmail.com>
2016-08-25 18:16:24 +0530

    modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => ENV['ENV_MAIL_HOST'],
     :port => 587,
-    :user_name => "ikegaya.dev@gmail.com",
-    :password => "ikegayadev1227",
+    :user_name => ENV['ENV_MAIL_USER'],
+    :password => ENV['ENV_MAIL_PASS'],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
